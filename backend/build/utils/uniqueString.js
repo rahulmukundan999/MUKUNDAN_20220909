7f49ff6e630cf967dd0f2d8f92b1e7ed889890c7
rahul <rahul.mukundan@nuri.com>
2022-09-09 20:09:32 +0200

    removed build

--- backend/build/utils/uniqueString.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateUniqueString = void 0;
-var generateUniqueString = function () {
-    return Date.now().toString(36) + Math.random().toString(36).substring(2);
-};
-exports.generateUniqueString = generateUniqueString;
